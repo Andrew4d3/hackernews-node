976c5144a0558b4738e9b1e9cd1e058e09d11e79
Andrew4d3 <andrew4d3@gmail.com>
2021-04-30 23:20:54 -0400

    server done

--- src/index.js
+++ src/index.js
@@ -1,38 +1,53 @@
 const fs = require("fs");
 const path = require("path");
-const { ApolloServer } = require("apollo-server");
+const { ApolloServer, PubSub } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
+const { getUserId } = require("./utils");
+const Query = require("./resolvers/Query");
+const Mutation = require("./resolvers/Mutation");
+const User = require("./resolvers/User");
+const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
 
 const prisma = new PrismaClient();
+const pubsub = new PubSub();
 
 // 2
 const resolvers = {
-   Query: {
-      info: () => "Hello World",
-      feed: async (_parent, _args, context) => {
-         return context.prisma.link.findMany();
-      },
-   },
-   Mutation: {
-      // 2
-      post: (_parent, args, context, info) => {
-         const newLink = context.prisma.link.create({
-            data: {
-               url: args.url,
-               description: args.description,
-            },
-         });
-         return newLink;
-      },
-   },
+   Query,
+   Mutation,
+   Subscription,
+   User,
+   Link,
+   Vote,
 };
 
 // 3
 const server = new ApolloServer({
    typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
    resolvers,
-   context: {
-      prisma,
+   context: ({ req }) => {
+      return {
+         ...req,
+         prisma,
+         pubsub,
+         userId: req && req.headers.authorization ? getUserId(req) : null,
+      };
+   },
+   subscriptions: {
+      onConnect: (connectionParams) => {
+         if (connectionParams.authToken) {
+            return {
+               prisma,
+               userId: getUserId(null, connectionParams.authToken),
+            };
+         } else {
+            return {
+               prisma,
+            };
+         }
+      },
    },
 });
 
